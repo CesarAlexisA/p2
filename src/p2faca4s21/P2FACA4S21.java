352135526832b3f4c325c5bc52a3d25ccd045b68
52551 <52551@LAPTOP-CT8EMV28>
2023-05-25 16:51:54 -0600

    cesar alexis actualizacion de tranajp y lectura de archivos

---
+++ src/p2faca4s21/P2FACA4S21.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2faca4s21;
+
+/**
+ *
+ * @author Admin
+ */
+public class P2FACA4S21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new JFPrincipal().setVisible(true);
+        
+    }
+    
+}
